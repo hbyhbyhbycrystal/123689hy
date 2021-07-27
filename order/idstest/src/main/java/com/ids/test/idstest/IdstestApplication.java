2724eacaf08d37122b097178a2e15ae16bc10845
huangbaoying <huangbaoying@cmhi.chinamobile.com>
2021-07-27 18:00:55 +0800

    oct:rem idstest

---
+++ order/idstest/src/main/java/com/ids/test/idstest/IdstestApplication.java
@@ -0,0 +1,13 @@
+package com.ids.test.idstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdstestApplication.class, args);
+    }
+
+}
---
+++ order/idstest/src/test/java/com/ids/test/idstest/IdstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ids.test.idstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
